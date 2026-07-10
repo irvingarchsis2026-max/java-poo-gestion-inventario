9d87595bcf281b6937aca7346d02ca434e26be98
Irving Castillo <irvingarchsis2026@gmail.com>
2026-07-10 00:50:29 -0500

    Add Product and Main classes
    Initial Java OOP structure for inventory project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Inventory management system - initial version");
+        ArrayList<Product> inventory = new ArrayList<>();
+
+        inventory.add(new Product(1, "Cement", "Materials", 32.50, 20));
+        inventory.add(new Product(2, "Steel Rod", "Materials", 45.00, 15));
+        inventory.add(new Product(3, "Paint Bucket", "Finishes", 28.90, 10));
+
+        System.out.println("Inventory Management System");
+        System.out.println("---------------------------");
+
+        for (Product product : inventory) {
+            System.out.println(product);
+        }
     }
 }
---
+++ src/Product.java
@@ -0,0 +1,51 @@
+public class Product {
+    private int id;
+    private String name;
+    private String category;
+    private double price;
+    private int stock;
+
+    public Product(int id, String name, String category, double price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public double getTotalValue() {
+        return price * stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                ", totalValue=" + getTotalValue() +
+                '}';
+    }
+}
